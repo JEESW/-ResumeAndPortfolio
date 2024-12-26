0bc7a5b235bd3f75acb8199de2a66d12d76d58d9
JEESW <jeesw1221@gmail.com>
2024-12-26 23:54:45 +0900

    feat: Initial Commit

---
+++ backend/Resume-And-Portfolio/src/main/java/com/example/resumeandportfolio/ResumeAndPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeandportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAndPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAndPortfolioApplication.class, args);
+	}
+
+}
