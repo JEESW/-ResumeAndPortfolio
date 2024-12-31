14db78c92e02bae431f9d63df75bb38b04dce435
JEESW <jeesw1221@gmail.com>
2024-12-31 15:57:13 +0900

    feat: Tailwind 설정

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header />
+        <main className="mt-8">
+          <section id="home" className="h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold">Welcome to Home Page</h1>
+          </section>
+          <section id="resume" className="h-screen flex items-center justify-center bg-gray-200">
+            <h1 className="text-4xl font-bold">Resume Page</h1>
+          </section>
+          <section id="portfolio" className="h-screen flex items-center justify-center bg-gray-300">
+            <h1 className="text-4xl font-bold">Portfolio Page</h1>
+          </section>
+        </main>
+      </div>
   );
 }
 
