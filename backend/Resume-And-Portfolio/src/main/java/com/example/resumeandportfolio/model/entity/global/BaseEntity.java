ba60402edefa14b4166f931c6e2fb801462947bb
JEESW <jeesw1221@gmail.com>
2025-01-06 00:21:54 +0900

    feat: BaseEntity 작성

---
+++ backend/Resume-And-Portfolio/src/main/java/com/example/resumeandportfolio/model/entity/global/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.resumeandportfolio.model.entity.global;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * BaseEntity
+ *
+ * @author Ji-Seungwoo
+ * @version 1.0
+ */
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
