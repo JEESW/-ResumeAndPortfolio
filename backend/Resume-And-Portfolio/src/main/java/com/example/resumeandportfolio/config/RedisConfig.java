0b89998e518e240e955909c0e93951983054bfab
JEESW <jeesw1221@gmail.com>
2025-01-14 22:36:29 +0900

    refactor: RedisConfig에 비밀번호 추가

--- backend/Resume-And-Portfolio/src/main/java/com/example/resumeandportfolio/config/RedisConfig.java
+++ backend/Resume-And-Portfolio/src/main/java/com/example/resumeandportfolio/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.resumeandportfolio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -17,9 +18,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379); // Redis 호스트와 포트 설정
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        factory.setPassword(redisPassword);
+        return factory;
     }
 
     @Bean
